6c59ef864c71076c46651ec1e7474307f13c735d
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-06-29 17:24:17 -0500

    step 1 done

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,4 +1,14 @@
 export class Satellite {
 
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        
+    }
 
 }
